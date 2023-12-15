677f5b74c81034a7e79e44c6825ca88a8242cdbb
John Joseph II <jackjoseph7447@gmail.com>
2023-12-14 23:58:46 -0500

    Added Code Test

---
+++ Encryption/EncryptionClassLibrary/PasswordHasher.cs
@@ -0,0 +1,88 @@
+﻿namespace Project5.Models
+{
+    using System;
+    using Konscious.Security.Cryptography;
+    using System.Linq;
+    using System.Text;
+
+    public class PasswordHasher
+    {
+        private readonly int DegreeOfParallelism = 8;
+        private readonly int MemorySize = 65536;
+        private readonly int Iterations = 4;
+
+        public string HashPassword(string password)
+        {
+            // Generate a random salt
+            byte[] salt = new byte[16];
+            using (var rng = new System.Security.Cryptography.RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(salt);
+            }
+
+            // Convert the string password to a byte array
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            // Hash the password using Argon2
+            using (var hasher = new Argon2id(passwordBytes)) //testing changes
+            {
+                hasher.Salt = salt;
+                hasher.DegreeOfParallelism = DegreeOfParallelism;
+                hasher.MemorySize = MemorySize; // in KB. This size allows for it to be more expensive tech wise to try and crack the hash
+                hasher.Iterations = Iterations; //This also helps with the slowing process of brute-force attacks.
+                                                //typically, this number can be as high as thousands or tens of thousands.
+
+                byte[] hash = hasher.GetBytes(32); // 32-byte hash
+                byte[] hashWithSalt = salt.Concat(hash).ToArray();
+
+                return Convert.ToBase64String(hashWithSalt);
+            }
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            try
+            {
+                // get salt and hash from stored password
+                byte[] hashWithSalt = Convert.FromBase64String(hashedPassword);
+
+                // get hash size
+                int hashSize = 32; //from hash in HashPassword
+
+                // makes sure the array is long enough
+                if (hashWithSalt.Length < 16 + hashSize)
+                {
+                    return false;
+                }
+
+                // get stored hash from hashWithSalt array
+                byte[] storedHash = new byte[hashSize];
+                Array.Copy(hashWithSalt, 16, storedHash, 0, hashSize);
+
+                // make string password to byte array
+                byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+                // Hash the provided password using the stored salt
+                using (var hasher = new Argon2id(passwordBytes))
+                {
+                    byte[] salt = hashWithSalt.Take(16).ToArray();
+
+                    hasher.Salt = salt;
+                    hasher.DegreeOfParallelism = DegreeOfParallelism;
+                    hasher.MemorySize = MemorySize;
+                    hasher.Iterations = Iterations;
+
+                    byte[] newHash = hasher.GetBytes(hashSize); // Adjust this size based on your hash size
+
+                    // Compare the stored hash with the newly generated hash
+                    return newHash.SequenceEqual(storedHash);
+                }
+            }
+            catch (Exception)
+            {
+                // Handle the exception appropriately (log, rethrow, etc.)
+                return false;
+            }
+        }
+    }
+}
